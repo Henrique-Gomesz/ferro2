4d89279ad50b2d739323805c4390943fde93b6b4
Henrique-rubcube <henrique.gomes@rubcube.com>
2023-02-16 19:27:35 -0300

    Update pais class

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Ferramentas computacionais II 
+# Ferramentas computacionais II ☕
 
 <!---Esses são exemplos. Veja https://shields.io para outras pessoas ou para personalizar este conjunto de escudos. Você pode querer incluir dependências, status do projeto e informações de licença aqui--->  
 
 
-<img src="https://cdn-icons-png.flaticon.com/512/226/226777.png" alt="Java Icon">
-
-> 
+> # Tópicos Abordados
+> - [x] Orientação a Objeto
+> - [] Ferramenta Astah
+> - [] JavaFx
--- Revisao/Parte1/src/Pais.java
+++ Revisao/Parte1/src/Pais.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Pais {
 
@@ -8,13 +9,13 @@ public class Pais {
     private Pais[] vizinhos;
     private int tlVizinho;
 
-    public Pais(String codigoIso, String nome, int populacao, double dimensao) {
+    public Pais(String codigoIso, String nome, int populacao, double dimensao, Pais[] vizinhos, int tlVizinho) {
         this.codigoIso = codigoIso;
-        this.dimensao = dimensao;
         this.nome = nome;
         this.populacao = populacao;
-        this.tlVizinho = 0;
-        this.vizinhos = new Pais[5];
+        this.dimensao = dimensao;
+        this.vizinhos = vizinhos;
+        this.tlVizinho = tlVizinho;
     }
 
     // GET
@@ -78,18 +79,42 @@ public class Pais {
         return this.populacao / this.dimensao;
     }
 
-    public Pais[] getSameVizinhos(Pais p){
-        int tl=0;
-        for(int i=0;i<this.tlVizinho;i++)
-        {
-            for(int j =0; j< p)
+    public ArrayList<Pais> getSameVizinhos(Pais p) {
+        ArrayList<Pais> arrayPais = new ArrayList<>();
+        for (int i = 0; i < this.tlVizinho; i++) {
+            for (int j = 0; j < p.vizinhos.length; j++) {
+                if (this.vizinhos[i].equals(p.vizinhos[j])) {
+                    if (!arrayPais.contains(this.vizinhos[i]))
+                        arrayPais.add(this.vizinhos[i]);
+                }
+            }
         }
+        return arrayPais;
     }
+
     public static void main(String[] args)
             throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("asdasd");
-        System.out.println("macaco");
+        // Criando alguns objetos Pais
+        Pais[] brasilVizinhos = new Pais[3];
+        brasilVizinhos[0] = new Pais("UY", "Uruguai", 3477000, 176215, new Pais[] {}, 0);
+        brasilVizinhos[1] = new Pais("AR", "Argentina", 44938712, 2780400, new Pais[] {}, 0);
+        brasilVizinhos[2] = new Pais("PY", "Paraguai", 7152703, 406752, new Pais[] {}, 0);
 
+        Pais brasil = new Pais("BR", "Brasil", 209500000, 8515767, brasilVizinhos, 3);
+
+        Pais argentina = new Pais("AR", "Argentina", 44938712, 2780400, null, 0);
+        Pais uruguai = new Pais("UY", "Uruguai", 3477000, 176215, new Pais[] { brasil, argentina }, 2);
+        Pais chile = new Pais("CL", "Chile", 19116209, 756096.3, new Pais[] { uruguai, argentina }, 2);
+
+        // Obtendo a lista de países que são vizinhos comuns de Chile e Uruguai
+        ArrayList<Pais> vizinhosComuns = chile.getSameVizinhos(brasil);
+        System.out.println("Vizinhos comuns de Chile e Uruguai: ");
+        for (Pais vizinho : vizinhosComuns) {
+            System.out.println(vizinho.getNome());
+        }
+
+        // Calculando a densidade populacional do Chile
+        double densidadePopulacional = chile.getDensidadePopulacional();
+        System.out.println("Densidade populacional do Chile: " + densidadePopulacional + " habitantes por km².");
     }
 }
